382d6f9f991055718d8c11898d35781a5c2f6aad
Sarpwus <owusu.sarpong@outlook.com>
2021-06-26 11:57:47 +0000

    first commit using google colab

---
+++ firstpython.py
@@ -0,0 +1,7 @@
+# learning how to use git in Google colab
+# to clone a repo, add new file, make changes
+# commit them and finally push them to Github
+
+print('Hey Google Colab')
+print('Yes, how may I help you?')
+print('help me commit my first changes to github')
